678c6ec0a41dcacc582eb062961703119c6526db
576973561@qq.com <576973561@qq.com>
2021-02-08 15:46:18 +0800

    second commi

--- src/main/java/com/learn/nio/one/server.java
+++ src/main/java/com/learn/nio/one/server.java
@@ -1,9 +1,12 @@
 package com.learn.nio.one;
 
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 
 public class server {
     public static void main(String[] args) throws Exception{
@@ -20,6 +23,26 @@ public class server {
         //6.使用选择器轮询已就绪的事件
         while (selector.select()>0){
             //7.获取选择器的所有就绪好的事件
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()){
+                SelectionKey sk = iterator.next();
+                if(sk.isAcceptable()){
+                    SocketChannel sChannel = ssChannel.accept();
+                    sChannel.configureBlocking(false);
+                    sChannel.register(selector,SelectionKey.OP_READ);
+                }else if(sk.isReadable()){
+                    SocketChannel channel = (SocketChannel) sk.channel();
+                    ByteBuffer buf = ByteBuffer.allocate(1024);
+                    int len = 0;
+                    while ((len=channel.read(buf))>0){
+                        buf.flip();
+                        System.out.println(new String(buf.array(), 0, len));
+                        buf.clear();
+                    }
+                }
+                iterator.remove();
+
+            }
         }
     }
 }
